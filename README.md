533bc5af92b98196bf10067e0ff6d820f39f25df
Monsieur <christopher.chhim123@gmail.com>
2024-07-04 00:20:08 -0700

    Added CSS

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # Weather-Dashboard
+
+## Description
+
+I like to travel so I was motivated to design a website where I can see the weather forecast for the present date as well as the next 5 days. I built this project to see which day would be most suitable for me to travel to my destination. This helps me in preventing having to check my phone or open multiple apps whereas I can check whichever city I so choose. This convenient weather app helps me plan ahead of time in case of any undesirable weather conditions. I learned how to use APIs to fetch real time data for web development purposes.
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+Download an IDE. 
+Use OpenWeatherMapAPI to fetch desired APIs.
+ 
+## Usage
+
+Type a city of your choice in the search bar. The API will then fetch real-time data to provide you the temperature, wind speed, and humidity of desired city.
+
+## Credits
+
+I would like to thank the following individuals for helping me design this project.
+
+Bootcamp teacher assistant: Omar Daghestani
+Bootcamp instructor: Rashaad Randall
+Bootcamp tutors: Andres Jimenez & Carver
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,39 @@
 let apiKey="25382a741e6bced20fc1f59a53126a82";
 let citySearch = document.getElementById(`citySearch`);
 let sectionHistory = document.getElementById(`history`);
+let weatherForecast = document.getElementById(`forecast`);
+
+let temperature = document.getElementById(`temperature`);
+let wind = document.getElementById(`wind`);
+let humidity = document.getElementById(`humidity`);
+let date = document.querySelector(`#date`);
+
+let temperature_0 = document.getElementById(`temperature_0`);
+let wind_0 = document.getElementById(`wind_0`);
+let humidity_0 = document.getElementById(`humidity_0`);
+let date_0 = document.getElementById(`date_0`);
+
+let temperature_1 = document.getElementById(`temperature_1`);
+let wind_1 = document.getElementById(`wind_1`);
+let humidity_1 = document.getElementById(`humidity_1`);
+let date_1 = document.getElementById(`date_1`);
+
+let temperature_2 = document.getElementById(`temperature_2`);
+let wind_2 = document.getElementById(`wind_2`);
+let humidity_2 = document.getElementById(`humidity_2`);
+let date_2 = document.getElementById(`date_2`);
+
+let temperature_3 = document.getElementById(`temperature_3`);
+let wind_3 = document.getElementById(`wind_3`);
+let humidity_3 = document.getElementById(`humidity_3`); 
+let date_3 = document.getElementById(`date_3`);
+
+let temperature_4 = document.getElementById(`temperature_4`);
+let wind_4 = document.getElementById(`wind_4`);
+let humidity_4 = document.getElementById(`humidity_4`);
+let date_4 = document.getElementById(`date_4`);
+
+
 
 function getHistory(){
     let cityHistory = JSON.parse(localStorage.getItem("city")) || [];
@@ -12,17 +45,62 @@ function saveHistory(cityHistory){
     return;
 }
 
+function currentWeather(cityName){
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`)
+    .then((res)=> {
+        return res.json()
+    })
+    .then((data)=>{
+        console.log(data);
+        console.log(data.wind.speed)
+        console.log(data.main.temp)
+        console.log(data.main.humidity)
+        temperature.textContent = "Temp: " + data.main.temp + " °F";
+        wind.textContent = "Wind speed: " + data.wind.speed + " MPH";
+        humidity.textContent = "Humidity: " + data.main.humidity + " %";
+        date.textContent = cityName + " " + dayjs().format('MM/DD/YYYY')
+    })
+}
+
 function getWeather(lat, lon){
-    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
     .then((res)=> {
         return res.json()
     })
     .then((data)=>{
         console.log(data);
         console.log(data.list);
+        let pTag = document.createElement('p')
         for(let i=0; i<data.list.length; i+=7){
             console.log(data.list[i]);
+            console.log(data.list[i].wind.speed)
+            console.log(data.list[i].main.temp)
+            console.log(data.list[i].main.humidity)
         }
+        temperature_0.textContent = "Temp: " + data.list[7].main.temp + " °F";
+        wind_0.textContent = "Wind speed: " + data.list[7].wind.speed + " MPH";
+        humidity_0.textContent = "Humidity: " +data.list[7].main.humidity + " %";
+        date_0.textContent = dayjs().add(1, "days").format('MM/DD/YYYY')
+
+        temperature_1.textContent = "Temp: " + data.list[14].main.temp; + " °F";
+        wind_1.textContent = "Wind speed: " + data.list[14].wind.speed + " MPH";
+        humidity_1.textContent = "Humidity: " +data.list[14].main.humidity + " %";
+        date_1.textContent = dayjs().add(2, "days").format('MM/DD/YYYY')
+
+        temperature_2.textContent = "Temp: " + data.list[21].main.temp +  " °F";
+        wind_2.textContent = "Wind speed: " +  data.list[21].wind.speed + " MPH";
+        humidity_2.textContent = "Humidity: " +data.list[21].main.humidity + " %";
+        date_2.textContent = dayjs().add(3, "days").format('MM/DD/YYYY')
+        
+        temperature_3.textContent = "Temp: " + data.list[28].main.temp + " °F";
+        wind_3.textContent = "Wind speed: " +  data.list[28].wind.speed + " MPH";
+        humidity_3.textContent = "Humidity: " +data.list[28].main.humidity + " %";
+        date_3.textContent = dayjs().add(4, "days").format('MM/DD/YYYY')
+
+        temperature_4.textContent = "Temp: " + data.list[35].main.temp + " °F";
+        wind_4.textContent = "Wind speed: " +  data.list[35].wind.speed + " MPH";
+        humidity_4.textContent = "Humidity: " +data.list[35].main.humidity + " %";
+        date_4.textContent = dayjs().add(5, "days").format('MM/DD/YYYY')
     })
 }
 
@@ -38,6 +116,7 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey
     console.log(lat);
     console.log(lon);
     getWeather(lat, lon);
+    currentWeather(cityName);
 })
 }
 
@@ -63,4 +142,14 @@ function renderHistoryBtn(){
         sectionHistory.appendChild(buttonTag);
     }
 }
+
+
+
+// function weatherCards(){
+//     let header = document.createElement(" div ")
+//     header.textContent = 
+// }
+
 renderHistoryBtn();
+
+
